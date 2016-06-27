a5591cfc0a7d2dee71a2a250db06d67c9507a7c2
Joshua Walker <jshwlkr@umich.edu>
2016-06-27 10:33:03 -0400

    link check via gulp + travis

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,26 @@
+/*global require console*/
+
 var gulp = require('gulp');
 var check = require('check-pages');
-var server = require('gulp-webserver');
-
+var connect = require('gulp-connect');
 
 gulp.task('webserver', function() {
-  gulp.src('./')
-    .pipe(server({
-    livereload: true,
-    directoryListing: true,
-    open: true
-    }));
+  connect.server({
+  root: './',
+  port: 8888,
+  livereload: false
+  });
 });
 
-gulp.task('default', ['webserver', 'runcheck'], function() {});
+gulp.task('default', ['webserver', 'runcheck'], function() {
+
+  connect.serverClose();
+});
 
 gulp.task('runcheck', function(callback) {
   var options = {
   pageUrls: [
-  'http://localhost:8000/index.html'
+  'http://localhost:8888/'
   ],
   checkLinks: true,
   maxResponseTime: 500
